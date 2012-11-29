32b16a29d3e52c54ad0c05fac4f4ad79d499e1b4
Tim Whelan <tim@suntail.net>
2012-11-29 14:38:39 +0200

    Initial import

---
+++ include/sfalib.h
@@ -0,0 +1,28 @@
+/*
+ */
+
+#if !defined(_SFALIB_H_)
+#define _SFALIB_H_
+
+class SFAOutput;
+
+class SFANode
+{
+};
+
+class SFAEdge
+{
+ public:
+ protected:
+};
+
+class SFA
+{
+ protected:
+    SFAAlphabet m_alphabet;
+    SFAAlphabet m_characters;
+
+    
+};
+
+#endif // _SFALIB_H_
---
+++ lib/sfalib.cpp
@@ -0,0 +1,5 @@
+/*
+ */
+
+#include <sfalib.h>
+
